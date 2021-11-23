393b5f1a6693dbf8abc522f708d4f00cc5a301d6
Johan <57840332+johannasendi@users.noreply.github.com>
2021-11-24 07:25:47 +0900

    first commit

---
+++ app/src/main/java/com/example/convert/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.convert;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginbtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 replaceFragment(new conversi());
+            }
+
+            private void replaceFragment(Fragment fragment) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, fragment);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+}
